95949ffbf6042a5a0dea3a220dd41f5d0c65d305
Nathan Schuett <nschuett@gmail.com>
2013-03-20 12:30:29 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeller::Application.config.secret_token = '9bad0cbc45e38bae859f2d9f6b92ac7577ce84f0b5baf352779ab6873d94b06b61baecaf6af1f50d322ce553eadeb2a060e00c6c0dc1338dfa6e32af2920ea2c'
